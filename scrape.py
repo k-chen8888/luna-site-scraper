67987dada3662b6ab442dbeac64f34c8c77cd8d2
Kevin Chen <k.chen8880@gmail.com>
2014-09-26 19:15:11 -0400

    Step forward, but still broken

--- scrape.py
+++ scrape.py
@@ -1,8 +1,35 @@
+# Beautiful Soup
 from bs4 import BeautifulSoup
 import requests
 
+# Wordpress
+from wordpress_xmlrpc import Client, WordPressPost
+from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+from wordpress_xmlrpc.methods.users import GetUserInfo
+
+
 r = requests.get("http://ameblo.jp/luna-luna-moonrise/")
 
-soup = BeautifulSoup(r.text)
+soup = BeautifulSoup(r.text) 
 
-print soup.prettify()
+
+# Get all relevant div elements
+div_list = soup.find_all("div", {"class": "entry"})
+
+
+# Dump each thing into a wordpress post
+new_posts = []
+i = 0
+for entry in div_list:
+	new_posts.append(WordPressPost())
+	new_posts[i].title = entry.find("h3").find("a").contents[0]
+	
+	new_posts[i].content = "***Begin Original Content Here***\n"
+	
+	for p in entry.find("div", {"class": "contents"}).find("div", {"class": "subContents"}).find("div", {"class": "subContentsInner"}).find_all("p", {"class": "MsoNormal"}):
+		new_posts[i].content += str(p)
+	
+
+	i += 1
+
+print new_posts
