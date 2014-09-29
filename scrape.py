3ceaeb2c2ecb9108a11119183d3c45fd8ea2a236
Kevin Chen <k.chen8880@gmail.com>
2014-09-29 01:28:35 -0400

    CLOSE FILES

--- scrape.py
+++ scrape.py
@@ -54,20 +54,28 @@ def post_to_wp(post_content, cred):
 
 if __name__ == "__main__":
 	try:
-		urls = open(sys.argv[1], 'r')
-		wp_info = open(sys.argv[2], 'r')
+		urlf = open(sys.argv[1], 'r')
+		urls = [x for x in urlf.readline()]
+		if len(urls) == 0:
+			print "Nothing to scrape"
+			return
 
+		wp_info = open(sys.argv[2], 'r')
 		wp_cred = [x for x in wp_info.readline()]
 		if len(wp_cred) != 3:
 			print "Malformed file"
 			return
-	
+		
+		# Close files
+		urlf.close()
+		wp_info.close()
+		
 	except IOError:
 		print "Not a file"
 		return
 	
 	# Get the actual content and dump it to Wordpress
-	for url in url.readline():
+	for url in urls:
 		if 'ameblo.jp' in url:
 			post_to_wp(get_post_list_ameblo(url), wp_cred)
 		elif 'sonymusic.co.jp' in url:
